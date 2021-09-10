5f1c0667c60fc59d4856c9959a9f4d87dafc0266
Cody Wattendorf <codywattendorf@Codys-MacBook-Air.local>
2021-09-09 21:11:14 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+y = "Test"
+x = "Cody"
+print("hello my name is cody")
+print("i'm using multiple print statements")
+print("This \n is \n a \n test")
+print("""using triple quotes""")
+print(x)
+print(x)
+print(y)
